b5f33db29d33d5de909a2a94c0663375e8ceed7f
sareth-13 <sareth.marquez@ucsp.edu.pe>
2026-05-17 20:48:34 -0500

    Update establo.sql

--- establo.sql
+++ establo.sql
@@ -5,8 +5,8 @@ CREATE TABLE USUARIO(
 id_usuario INT PRIMARY KEY AUTO_INCREMENT,
 id_empleado INT,
 nmbre_usuaio VARCHAR(50) UNIQUE NOT NULL,
-pasword_hash VARCHAR(225),
-activo BOOLEAN DEAULT TRUE,
+password_hash VARCHAR(225),
+activo BOOLEAN DEfAULT TRUE,
 FOREIGN KEY (id_empleado) REFERENCES EMPLEADO(id_empleado)
 );
 
@@ -24,7 +24,7 @@ activo BOOLEAN DEFAULT TRUE
 CREATE TABLE HISTORIAL_CORRAL(
 id_hstrial_crral INT PRIMARY KEY AUTO_INCREMENT,
 id_vaca INT,
-id corral INT,
+id_corral INT,
 fcha_entrda DATE,
 fcha_slida DATE,
 motivo VARCHAR(225),
@@ -47,7 +47,7 @@ fcha DATE,
 litros DECIMAL(5,2),
 grasa_prcntaje DECIMAL (5,2),
 solidos_totales_prcntaje DECIMAL (5,2),
-turno ENUN('MAÑANA','TARDE','NOCHE'),
+turno ENUM ('MAÑANA','TARDE','NOCHE'),
 FOREIGN KEY (id_vaca) REFERENCES VACA(id_vaca),
 FOREIGN KEY (id_lote_prdccion) REFERENCES LOTE_PRODUCCION(id_lote_prdccion)
 );
@@ -58,7 +58,7 @@ id_lote_prdccion INT,
 fcha DATE,
 litros_vendidos DECIMAL(8,2),
 precio_por_litro DECIMAL(6,2),
-total_venta DECIMAL(10,2)
+total_venta DECIMAL(10,2),
 comprador VARCHAR(100),
 FOREIGN KEY(id_lote_prdccion) REFERENCES LOTE_PRODUCCION(id_lote_prdccion)
 );
