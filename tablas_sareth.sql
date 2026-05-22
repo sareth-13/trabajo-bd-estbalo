ca0211d79c574975b59b10d5f99b4cda6536f649
sareth-13 <sareth.marquez@ucsp.edu.pe>
2026-05-22 09:33:31 -0500

    Update tablas_sareth.sql

--- tablas_sareth.sql
+++ tablas_sareth.sql
@@ -14,10 +14,11 @@ VALUES
 (4, 4),
 (5, 5);
 
-INSERT INTO USUARIO_ROL (id_usuario,id_rol)
+INSERT INTO USUARIO 
+(id_usuario, id_empleado, nmbre_usuario, pasword_hash, activo)
 VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5);
+(1, 1, 'jperez', 'hash123', 1),
+(2, 2, 'mgomez', 'hash456', 1),
+(3, 3, 'cramirez', 'hash789', 1),
+(4, 4, 'atorres', 'hash321', 1),
+(5, 5, 'lfernandez', 'hash654', 0);
