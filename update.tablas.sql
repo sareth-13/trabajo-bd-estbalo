71a6892ebf50478b9c365a8e46ad1b8338c20b89
joaquinojedavillafuerte <joaquin.ojeda.villafuerte@ucsp.edu.pe>
2026-05-25 21:13:12 -0500

    Update update.tablas.sql

--- update.tablas.sql
+++ update.tablas.sql
@@ -1,3 +1,14 @@
+-- UPDATE 1: Sareth - Aumento de salario a supervisores
 UPDATE EMPLEADO
 SET salario_base = salario_base * 1.10
 WHERE cargo = 'Supervisor';
+
+-- UPDATE 2: Luis - Actualizar stock de insumo
+UPDATE INSUMO
+SET stock_actual = stock_actual + 50
+WHERE id_insumo = 1;
+
+-- UPDATE 3: Joaquín - Cambiar estado de una vaca
+UPDATE VACA
+SET estado = 'Vendida'
+WHERE id_vaca = 4;
