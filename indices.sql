46bebea74decd9a875a114d46936f53d385be943
Sayra67 <luis.sayra@ucsp.edu.pe>
2026-06-16 22:32:08 -0500

    feat: indices.sql

--- indices.sql
+++ indices.sql
@@ -1,18 +1,49 @@
 USE establo;
 
--- PRODUCCIÓN
-CREATE INDEX idx_prod_vaca ON PRODUCCION_LECHE(id_vaca);
-CREATE INDEX idx_prod_fecha ON PRODUCCION_LECHE(fecha);
-CREATE INDEX idx_prod_vaca_fecha ON PRODUCCION_LECHE(id_vaca, fecha);
+CREATE INDEX idx_empleado_nombre
+ON EMPLEADO(nombre);
 
--- EVENTOS
-CREATE INDEX idx_evento_vaca ON EVENTO_SANITARIO(id_vaca);
+CREATE INDEX idx_empleado_apellido
+ON EMPLEADO(apellido);
 
--- COMPRAS
-CREATE INDEX idx_compra_proveedor ON COMPRA_INSUMO(id_proveedor);
+CREATE INDEX idx_vaca_estado
+ON VACA(estado);
 
--- HISTORIAL
-CREATE INDEX idx_historial_vaca ON HISTORIAL_CORRAL(id_vaca);
+CREATE INDEX idx_vaca_fecha_ingreso
+ON VACA(fecha_ingreso);
 
--- ÍNDICES DINÁMICOS (OPCIONAL)
-CREATE INDEX idx_dinamico_litros ON PRODUCCION_LECHE(litros);
+CREATE INDEX idx_corral_capacidad
+ON CORRAL(capacidad);
+
+CREATE INDEX idx_produccion_vaca
+ON PRODUCCION_LECHE(id_vaca);
+
+CREATE INDEX idx_produccion_lote
+ON PRODUCCION_LECHE(id_lote_prdccion);
+
+CREATE INDEX idx_produccion_fecha
+ON PRODUCCION_LECHE(fcha);
+
+CREATE INDEX idx_venta_lote
+ON VENTA_LECHE(id_lote_prdccion);
+
+CREATE INDEX idx_insumo_nombre
+ON INSUMO(nombre);
+
+CREATE INDEX idx_compra_proveedor
+ON COMPRA_INSUMO(id_proveedor);
+
+CREATE INDEX idx_compra_insumo
+ON COMPRA_INSUMO(id_insumo);
+
+CREATE INDEX idx_pago_empleado
+ON PAGO_EMPLEADO(id_empleado);
+
+CREATE INDEX idx_evento_vaca
+ON EVENTO_SANITARIO(id_vaca);
+
+CREATE INDEX idx_historial_vaca
+ON HISTORIAL_CORRAL(id_vaca);
+
+CREATE INDEX idx_historial_corral
+ON HISTORIAL_CORRAL(id_corral);
